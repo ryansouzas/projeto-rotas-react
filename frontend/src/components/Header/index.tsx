5d9bde26f30408f4d5a10ba7e20f65034090aed1
Ryan Souza <ryanssebadelhe@gmail.com>
2026-05-10 12:43:32 -0300

    feat: header layout

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+import homeIcon from "../../assets/home.svg";
+
+export default function Header() {
+  return (
+    <header className=" app-header">
+      <nav className="container app-nav app-nav-header">
+        <ul>
+          <li>Início</li>
+          <li>Produtos</li>
+          <li>Sobre Nós</li>
+        </ul>
+
+        <img src={homeIcon} alt="Home" />
+      </nav>
+    </header>
+  );
+}
